719d81a193c32c43af04dea686648fdaf1782466
Lean Saldivar <leanemmanuelsaldivar@gmail.com>
2026-07-19 15:19:36 +0800

    Created some files for later use

---
+++ frontend/src/components/button/button.tsx
@@ -0,0 +1,9 @@
+import "./button.scss"
+
+export const Button = () => {
+    return(
+        <button className="btn">
+            
+        </button>
+    )
+}
--- frontend/src/layout/inventoryLayout.tsx
+++ frontend/src/layout/inventoryLayout.tsx
@@ -1,8 +1,13 @@
 import './inventoryLayout.scss'
+import { Outlet } from "react-router-dom";
 
 export const InventoryLayout = () => {
     return (
-        <>
-        </>
+        <div className="main-container">
+            <p></p>
+            <div className="sticky-sidebar">
+            </div>
+            <Outlet/>
+        </div>
     )
 }
