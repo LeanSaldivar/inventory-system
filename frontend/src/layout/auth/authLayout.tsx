fd174dce282de18157d8bd06405cdd0292873df0
Lean Saldivar <leanemmanuelsaldivar@gmail.com>
2026-07-22 10:51:57 +0800

    Added outlet to manage both login and register pages

---
+++ frontend/src/layout/auth/authLayout.tsx
@@ -0,0 +1,27 @@
+import { Outlet } from "react-router-dom";
+import "./authLayout.scss"
+import { PillIcon } from "../../assets/svg/pillIcon";
+
+export const AuthLayout = () => {
+    return (
+        <div className="auth-container">
+            <header className="auth-header">
+
+                <div className="iconLogo">
+                    <PillIcon className="pillIconLogo" />
+                </div>
+
+                <h1>
+                    Reisa DrugStore & General Merchandise
+                </h1>
+
+                <span>
+                    Inventory Management System
+                </span>
+            </header>
+
+            
+            <Outlet />
+        </div>
+    )
+}
