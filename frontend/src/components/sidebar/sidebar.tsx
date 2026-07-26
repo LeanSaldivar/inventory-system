e3e396f81edd11f1ed677da1f5368ab319d16f1d
Lean Saldivar <leanemmanuelsaldivar@gmail.com>
2026-07-26 09:29:34 +0800

    moved the sidebar to its own dedicated folder

---
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,85 @@
+import { DashboardIcon } from '../../assets/svg/dashboardIcon';
+import { MenuIcon } from '../../assets/svg/menuIcon';
+import { PillIcon } from '../../assets/svg/pillIcon';
+import { ProductIcon } from '../../assets/svg/productIcont';
+import { ShoppingCartIcon } from '../../assets/svg/shoppingCartIcon';
+import { Link } from "react-router-dom";
+import "./sidebar.scss"
+import { LogoutBtn } from '../button/btn';
+import { LogoutIcon } from '../../assets/svg/logoutIcon';
+import { useState } from "react";
+
+export const Sidebar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <nav className={`sticky-sidebar ${collapsed ? "collapsed" : ""}`}>
+            <header className="sidebar-header">
+                <div className="logo-icon">
+                    <PillIcon width={20} height={20} className="mini-logo" />
+                </div>
+
+                <div className="text-wrapper">
+                    <div className="text">
+                        <p>
+                            Reisa Drugstore
+                        </p>
+
+                        <span>
+                            Pharmacy IMS
+                        </span>
+                    </div>
+
+
+                </div>
+                <div className="menu">
+                    <MenuIcon width={30} height={30} className="menu-icon" onClick={() => setCollapsed(!collapsed)} />
+                </div>
+            </header>
+
+            <hr />
+
+            <div className="header-pages">
+                <ul className="pages-container">
+                    <Link to="#" className="pages-item">
+                        <DashboardIcon width={30} height={30} className="pages-logo" />
+                        <span className="pages-url">Dashboard</span>
+                    </Link>
+                    <Link to="#" className="pages-item">
+                        <ProductIcon width={25} height={25} className="pages-logo" />
+                        <span className="pages-url">Products</span>
+                    </Link>
+                    <Link to="#" className="pages-item">
+                        <ShoppingCartIcon width={25} height={25} className="pages-logo" />
+                        <span className="pages-url">Sales/POS</span>
+                    </Link>
+                </ul>
+            </div>
+
+
+
+            <div className="logout">
+                <hr />
+
+                <div className="profile">
+                    <div className="initials">
+                        MS
+                    </div>
+
+                    <div className="text">
+                        <p>
+                            Maria Santos
+                        </p>
+
+                        <span>
+                            Pharmacist
+                        </span>
+                    </div>
+                </div>
+
+                <div className="logout-btn">
+                    <LogoutBtn icon={<LogoutIcon width={20} height={20} />} text={"Logout"} />
+                </div>
+            </div>
+        </nav>
+    )
+}
