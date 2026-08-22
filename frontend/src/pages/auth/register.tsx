c00d7d3d1fa24d887284aa36d402a42a4bf8d4c9
Lean Saldivar <leanemmanuelsaldivar@gmail.com>
2026-08-22 14:44:21 +0800

    register integration logic

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -1,11 +1,51 @@
 import { Link } from "react-router-dom";
 import AuthBtn from "../../components/button/btn";
 import "./auth.scss"
+import { useState } from "react";
+import type { RegisterRequest } from "../../types/user";
+import { register } from "../../api/authService";
 
 export const Register = () => {
+    const [formData, setFormData] = useState<RegisterRequest>({
+        userName: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            setError('Passwords do not match.');
+            return;
+        }
+
+        setIsLoading(true);
+        try {
+            const response = await register(formData);
+            console.log(response.data);
+        } catch (err) {
+            setError('Registration failed. Please try again.');
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+
     return (
         <>
-            <form action="" className="login-form">
+            <form onSubmit={handleRegister} className="login-form">
                 <header className="form-header">
                     <h2>
                         Welcome Back!
@@ -25,8 +65,24 @@ export const Register = () => {
                     className="userName"
                     id="userName"
                     placeholder="John Doe"
+                    onChange={handleChange}
                     required
                 />
+
+                <label htmlFor="email">
+                    EMAIL
+                </label>
+                <input
+                    type="text"
+                    name="email"
+                    className="email"
+                    id="email"
+                    placeholder="please enter a valid email"
+                    onChange={handleChange}
+                    required
+                />
+
+
 
                 <label htmlFor="password">
                     PASSWORD
@@ -35,26 +91,35 @@ export const Register = () => {
                     type="password"
                     name="password"
                     id="password"
+                    onChange={handleChange}
                     required
                 />
 
-                <label htmlFor="password">
+                <label htmlFor="confirmPassword">
                     CONFIRM PASSWORD
                 </label>
                 <input
-                    type="password"
-                    name="password"
-                    id="password"
+                    type="confirmPassword"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    onChange={handleChange}
                     required
                 />
 
-                <AuthBtn text="Create Account!" />
+                <AuthBtn
+                    text={isLoading ? 'Loading...' : 'Register'}
+                    disabled={isLoading}
+                    type="submit"
+                />
 
                 <div className="register-link">
                     <Link to="/auth/login" className="register">
                         Sign-in
                     </Link>
                 </div>
+
+                {error && <p className="text-danger">{error}</p>} {/* Render error message if exists */}
+
             </form>
         </>
     )
